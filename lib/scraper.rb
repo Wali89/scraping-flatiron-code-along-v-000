823505de4f765d1cdd5d03dfb6aff2499b9b92bb
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-09 12:20:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,9 +16,9 @@ class Scraper
     end
   end
 
-  
-  
+  def get_page
+
+  end
+
+
 end
-
-
-
